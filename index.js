773f617b0441cf757c144ce979dfa4de50f45f5f
ManuelPeva <127805450+ManuelPeva@users.noreply.github.com>
2023-07-24 19:38:31 -0600

    corrección de errores

---
+++ index.js
@@ -0,0 +1,49 @@
+const pokemonContainer = document.querySelector('.pokemon-container');
+
+function fetchPokemon(id) {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`) //link de la poke api
+        .then(res => res.json())
+        .then((data) => {
+            createPokemon(data);
+        })
+    //.then(data => console.log(data)) //mostrar resultado en consola
+}
+
+function fetchPokemons(number) {
+    for (let i = 1; i <= number; i++) {
+        fetchPokemon(i-1);
+    }
+}
+
+fetchPokemons(12); //numero de pokemones en la pagina
+
+function createPokemon(pokemon) { //creación de elementos 
+    const card = document.createElement('div');
+    card.classList.add('pokemon-block');
+
+    const spriteContainer = document.createElement('div');
+    spriteContainer.classList.add('img-conatiner');
+
+    const sprite = document.createElement('img');
+    sprite.src = pokemon.sprites.front_default
+  
+
+    spriteContainer.appendChild(sprite);
+
+    const number = document.createElement('p');
+    number.textContent = `#${pokemon.id.toString().padStart(3, 0)}`;
+
+
+    const name = document.createElement('p');
+    name.textContent = pokemon.name
+
+    card.appendChild(spriteContainer); //agregarlo a la carta imagen
+    card.appendChild(number); //numero
+    card.appendChild(name); //nombre
+
+    pokemonContainer.appendChild(card);
+    
+
+}
+
+fetchPokemon(12);
